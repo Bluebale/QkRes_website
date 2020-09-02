9a8995680f767afac0327dc4d6128e2275f0bf9e
Faseeh Ahmed <47794893+Bluebale@users.noreply.github.com>
2020-09-02 15:22:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+
 document.addEventListener('DOMContentLoaded',function(){
   $('.Search i').on('click',function(){
     $('.Search').toggleClass('active')
   })
 })
+ function scrollAppear() {
+   var box = document.querySelectorAll('.box');
+   var box2 = document.querySelectorAll('.box-2');
+   var screenPosition1 = window.innerHeight /1.3 ;
+   var screenPosition2 = window.innerHeight /1.2 ;
+   console.log('hello');
+   for(var i=0; i < box.length; i++) {
+     var boxPosition = box[i].getBoundingClientRect().top;
+     if(boxPosition < screenPosition1) {
+       box[i].classList.add('box-appear');
+     }
+   }
+   for(var i = 0; i < box2.length; i++) {
+     var boxPosition = box2[i].getBoundingClientRect().top;
+     if(boxPosition < screenPosition2) {
+       box2[i].classList.add('box-appear');
+     }
+   }
+ }
+
+ window.addEventListener('scroll',scrollAppear);
